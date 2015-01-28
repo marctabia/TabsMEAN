3cb372df970cda1d94baa5f85df3e6f95d87e919
marctabia <marctabia@google.com>
2015-01-28 23:38:14 +0800

    added settings for ejs templating, static files, and session; added ejs template file; added logo image for static file example

--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,4 @@
 module.exports = {
     // add development config options
+    sessionSecret: 'developmentSessionSecret'
 };
--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
+var config = require('./config');
 var express = require('express');
 var morgan = require('morgan');
 var compress = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 module.exports = function() {
     var app = express();
@@ -18,10 +20,19 @@ module.exports = function() {
     }));
     app.use(bodyParser.json());
     app.use(methodOverride());
-    
+
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret
+    }));
+
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
-        
+
     require('../app/routes/index.server.routes')(app);
+
+    app.use(express.static('./public'));
+
     return app;
 };
